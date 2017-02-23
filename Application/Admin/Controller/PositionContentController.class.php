b98b64ab28d336bc866f23797df922fc8ceb3b5c
zbk <1030988732@qq.com>
2017-02-23 09:37:33 +0800

    推荐位内容管理

--- Application/Admin/Controller/PositionContentController.class.php
+++ Application/Admin/Controller/PositionContentController.class.php
@@ -7,9 +7,22 @@ use Think\Controller;
 class PositionContentController extends CommonController {
 
     public function index(){
-        // 获取所有推荐位
-        // $positions = M('Position')->field('id,name,create_time,status')->select();
-        // $this -> assign('positions',$positions);
+        $conds = array();
+        $title = $_GET['title'];
+        if($title){
+            $conds['title'] = $title;
+            $this -> assign('title',$title);
+        }
+        if ($_GET['position_id']) {
+            $conds['position_id'] = intval($_GET['position_id']);
+            $this -> assign('position_id',$conds['position_id']);
+        }
+        //获取所有状态为正常的推荐位栏目
+        $positions = M('Position')->field('id,name,create_time,status')
+        ->where(array('status'=>1))
+        ->order('id desc')
+        ->select();
+        $this -> assign('positions',$positions);
     	$this->display();
     }
 }
